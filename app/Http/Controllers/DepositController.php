200034986efec2f7fa5d00dbf21c9d739a6488b5
nyatado98 <danndong080@gmail.com>
2023-05-30 18:05:32 +0300

    controllers

--- app/Http/Controllers/DepositController.php
+++ app/Http/Controllers/DepositController.php
@@ -11,13 +11,13 @@ class DepositController extends Controller
     public function stk(){
         $mpesa = new \Safaricom\Mpesa\Mpesa();
 
-        $BusinessShortCode = '174379';
+        $BusinessShortCode = 174379;
         $LipaNaMpesaPassKey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
         $TransactionType = 'CustomerPaybillOnline';
-        $Amount = '1';
-        $PartyA = '254726585782';
-        $PartyB = '174379';
-        $PhoneNumber = '254726585782';
+        $Amount = 1;
+        $PartyA = 254726585782;
+        $PartyB = 174379;
+        $PhoneNumber = 254726585782;
         $CallBackUrl = 'https://mydomain.com/path';
         $AccountReference = 'Bettings company dans';
         $TransactionDesc = 'Transaction desc';
